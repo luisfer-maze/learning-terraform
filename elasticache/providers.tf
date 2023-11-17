41981cfa1cab689d285a19691851d959437dfbfe
Luis Fernando Maldonado Zelada <maldonado.luisfer@gmail.com>
2023-11-17 20:09:23 +0000

    providers

---
+++ elasticache/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">=1.4.5"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0, < 5.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
