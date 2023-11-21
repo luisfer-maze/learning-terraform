472c89f5d52eaa888b567f71d9454d979d493886
Luis Fernando Maldonado Zelada <maldonado.luisfer@gmail.com>
2023-11-21 18:01:08 +0000

    desperate intent to make it work

--- lambda/main.tf
+++ lambda/main.tf
@@ -5,6 +5,13 @@ locals {
 }
 
 resource "null_resource" "function_binary" {
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+
+    command = "rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz && export PATH=$PATH:/usr/local/go/bin && go version"
+  }
+
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
 
